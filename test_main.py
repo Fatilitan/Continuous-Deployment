59adf8c27c16b62e04b9ab1f739ddcb05ab598f3
Fatilitan <danielekstedt01@gmail.com>
2023-12-12 15:54:07 +0100

    Test file gefixt

--- test_main.py
+++ test_main.py
@@ -10,17 +10,17 @@ def client():
         yield client
 
 def test_addition(client: FlaskClient):
-    response = client.post('/calculate', data={'num1': '2', 'num2': '3', 'operation': 'add'})
-    assert b'Result: 5.0' in response.data
+    response = client.post('/calculate', data={'num1': '2', 'num2': '3', 'bewerking': 'optellen'})
+    assert b'Resultaat: 5.0' in response.data
 
 def test_subtraction(client: FlaskClient):
-    response = client.post('/calculate', data={'num1': '5', 'num2': '3', 'operation': 'subtract'})
-    assert b'Result: 2.0' in response.data
+    response = client.post('/calculate', data={'num1': '5', 'num2': '3', 'bewerking': 'aftrekken'})
+    assert b'Resultaat: 2.0' in response.data
 
 def test_multiplication(client: FlaskClient):
-    response = client.post('/calculate', data={'num1': '4', 'num2': '6', 'operation': 'multiply'})
-    assert b'Result: 24.0' in response.data
+    response = client.post('/calculate', data={'num1': '4', 'num2': '6', 'bewerking': 'vermenigvuldigen'})
+    assert b'Resultaat: 24.0' in response.data
 
 def test_division(client: FlaskClient):
-    response = client.post('/calculate', data={'num1': '8', 'num2': '2', 'operation': 'divide'})
-    assert b'Result: 4.0' in response.data
+    response = client.post('/calculate', data={'num1': '8', 'num2': '2', 'bewerking': 'delen'})
+    assert b'Resultaat: 4.0' in response.data
